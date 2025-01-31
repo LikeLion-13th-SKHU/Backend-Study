bea2d64df67140a764c6a33637f8a75531559dd5
ckr8305 <ckr8305@naver.com>
2025-01-31 15:14:06 +0900

    레벨2/9장

---
+++ kyulim/springboot-developer/src/main/java/com/example/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.springbootdeveloper.repository;
+
+import com.example.springbootdeveloper.domain.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
