3c8f1dcc012a715b55e7ae1cc7d77cf4ccd8da4a
Junyeong-An <2117ab@gmail.com>
2025-02-02 00:48:33 +0900

    9장 제외된 파일 추가

---
+++ junyeong/springboot-developer/src/main/java/me/junyeong/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.junyeong.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
